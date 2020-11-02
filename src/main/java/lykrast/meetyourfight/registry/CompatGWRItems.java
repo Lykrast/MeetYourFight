8adefd802ebdafd1e495f6a2d277bf7e04e62fc0
Lykrast <aymericpm@gmail.com>
2020-11-02 17:35:29 +0100

    Updated forge and brought balance on GWR compat

--- src/main/java/lykrast/meetyourfight/registry/CompatGWRItems.java
+++ src/main/java/lykrast/meetyourfight/registry/CompatGWRItems.java
@@ -11,12 +11,12 @@ public class CompatGWRItems {
 	public static void registerItems(IForgeRegistry<Item> reg) {
 		//Does not have the increased projectile speed of the sniper for more reliable hit through walls
 		phantasmalRifle = ModItems.initItem(reg, 
-				new PhantasmalRifle(ModItems.noStack().maxDamage(5276), 0, 2.5, 22, 0, 22)
+				new PhantasmalRifle(ModItems.noStack().maxDamage(2376), 0, 1.6, 22, 0, 22)
 				.fireSound(lykrast.gunswithoutroses.registry.ModSounds.sniper)
 				.repair(() -> Ingredient.fromItems(ModItems.phantoplasm)), 
 				"phantasmal_rifle");
 		cocktailShotgun = ModItems.initItem(reg, 
-				new CocktailShotgun(ModItems.noStack().maxDamage(8273), 0, 0.3, 16, 5, 14, 7).ignoreInvulnerability(true)
+				new CocktailShotgun(ModItems.noStack().maxDamage(3473), 0, 0.3, 16, 5, 14, 7).ignoreInvulnerability(true)
 				.fireSound(lykrast.gunswithoutroses.registry.ModSounds.shotgun)
 				.repair(() -> Ingredient.fromItems(ModItems.fortunesFavor)), 
 				"cocktail_shotgun");
