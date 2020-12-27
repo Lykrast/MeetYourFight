d423c42124aabd1cc5480126524051d79c035ef4
Lykrast <aymericpm@gmail.com>
2020-12-27 22:42:59 +0100

    Swamp Mine model

---
+++ src/main/java/lykrast/meetyourfight/renderer/SwampMineModel.java
@@ -0,0 +1,102 @@
+package lykrast.meetyourfight.renderer;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+
+import lykrast.meetyourfight.entity.SwampMineEntity;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+
+// Made with Blockbench 3.7.4
+// Exported for Minecraft version 1.15
+// Paste this class into your mod and generate all required imports
+public class SwampMineModel extends EntityModel<SwampMineEntity> {
+	private final ModelRenderer spikes;
+	private final ModelRenderer bottombackright_r1;
+	private final ModelRenderer bottombackleft_r1;
+	private final ModelRenderer bottomfrontright_r1;
+	private final ModelRenderer bottomfrontleft_r1;
+	private final ModelRenderer topbackright_r1;
+	private final ModelRenderer topbackleft_r1;
+	private final ModelRenderer topfrontright_r1;
+	private final ModelRenderer topfrontleft_r1;
+	private final ModelRenderer bb_main;
+
+	public SwampMineModel() {
+		textureWidth = 64;
+		textureHeight = 32;
+
+		spikes = new ModelRenderer(this);
+		spikes.setRotationPoint(-6.0F, 10.0F, 6.0F);
+		
+
+		bottombackright_r1 = new ModelRenderer(this);
+		bottombackright_r1.setRotationPoint(0.0F, 12.0F, 0.0F);
+		spikes.addChild(bottombackright_r1);
+		setRotationAngle(bottombackright_r1, -2.3562F, -0.7854F, 0.0F);
+		bottombackright_r1.setTextureOffset(0, 0).addBox(-1.0F, -7.0F, -1.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+
+		bottombackleft_r1 = new ModelRenderer(this);
+		bottombackleft_r1.setRotationPoint(12.0F, 12.0F, 0.0F);
+		spikes.addChild(bottombackleft_r1);
+		setRotationAngle(bottombackleft_r1, -2.3562F, 0.7854F, 0.0F);
+		bottombackleft_r1.setTextureOffset(0, 0).addBox(-1.0F, -7.0F, -1.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+
+		bottomfrontright_r1 = new ModelRenderer(this);
+		bottomfrontright_r1.setRotationPoint(0.0F, 12.0F, -12.0F);
+		spikes.addChild(bottomfrontright_r1);
+		setRotationAngle(bottomfrontright_r1, 2.3562F, 0.7854F, 0.0F);
+		bottomfrontright_r1.setTextureOffset(0, 0).addBox(-1.0F, -7.0F, -1.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+
+		bottomfrontleft_r1 = new ModelRenderer(this);
+		bottomfrontleft_r1.setRotationPoint(12.0F, 12.0F, -12.0F);
+		spikes.addChild(bottomfrontleft_r1);
+		setRotationAngle(bottomfrontleft_r1, 2.3562F, -0.7854F, 0.0F);
+		bottomfrontleft_r1.setTextureOffset(0, 0).addBox(-1.0F, -7.0F, -1.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+
+		topbackright_r1 = new ModelRenderer(this);
+		topbackright_r1.setRotationPoint(0.0F, 0.0F, 0.0F);
+		spikes.addChild(topbackright_r1);
+		setRotationAngle(topbackright_r1, -0.7854F, -0.7854F, 0.0F);
+		topbackright_r1.setTextureOffset(0, 0).addBox(-1.0F, -7.0F, -1.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+
+		topbackleft_r1 = new ModelRenderer(this);
+		topbackleft_r1.setRotationPoint(12.0F, 0.0F, 0.0F);
+		spikes.addChild(topbackleft_r1);
+		setRotationAngle(topbackleft_r1, -0.7854F, 0.7854F, 0.0F);
+		topbackleft_r1.setTextureOffset(0, 0).addBox(-1.0F, -7.0F, -1.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+
+		topfrontright_r1 = new ModelRenderer(this);
+		topfrontright_r1.setRotationPoint(0.0F, 0.0F, -12.0F);
+		spikes.addChild(topfrontright_r1);
+		setRotationAngle(topfrontright_r1, 0.7854F, 0.7854F, 0.0F);
+		topfrontright_r1.setTextureOffset(0, 0).addBox(-1.0F, -7.0F, -1.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+
+		topfrontleft_r1 = new ModelRenderer(this);
+		topfrontleft_r1.setRotationPoint(12.0F, 0.0F, -12.0F);
+		spikes.addChild(topfrontleft_r1);
+		setRotationAngle(topfrontleft_r1, 0.7854F, -0.7854F, 0.0F);
+		topfrontleft_r1.setTextureOffset(0, 0).addBox(-1.0F, -7.0F, -1.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+
+		bb_main = new ModelRenderer(this);
+		bb_main.setRotationPoint(0.0F, 24.0F, 0.0F);
+		bb_main.setTextureOffset(0, 0).addBox(-8.0F, -16.0F, -8.0F, 16.0F, 16.0F, 16.0F, 0.0F, false);
+	}
+
+	@Override
+	public void setRotationAngles(SwampMineEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
+		//previously the render function, render code was moved to a method below
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		spikes.render(matrixStack, buffer, packedLight, packedOverlay);
+		bb_main.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
--- src/main/java/lykrast/meetyourfight/renderer/SwampMineRenderer.java
+++ src/main/java/lykrast/meetyourfight/renderer/SwampMineRenderer.java
@@ -1,41 +1,45 @@
 package lykrast.meetyourfight.renderer;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 
+import lykrast.meetyourfight.MeetYourFight;
 import lykrast.meetyourfight.entity.SwampMineEntity;
-import net.minecraft.block.Blocks;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.TNTMinecartRenderer;
-import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.util.math.MathHelper;
 
 public class SwampMineRenderer extends EntityRenderer<SwampMineEntity> {
-	// TODO For now it's just copy pasted TNT
+	private static final ResourceLocation TEXTURE = MeetYourFight.rl("textures/entity/swampmine.png");
+	private final EntityModel<SwampMineEntity> model = new SwampMineModel();
 
 	public SwampMineRenderer(EntityRendererManager renderManagerIn) {
 		super(renderManagerIn);
-		this.shadowSize = 0.5F;
+		shadowSize = 0.5F;
 	}
 
 	@Override
 	public void render(SwampMineEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
 		matrixStackIn.push();
-		matrixStackIn.translate(0.0D, 0.5D, 0.0D);
-
-		matrixStackIn.rotate(Vector3f.YP.rotationDegrees(-90.0F));
-		matrixStackIn.translate(-0.5D, -0.5D, 0.5D);
-		matrixStackIn.rotate(Vector3f.YP.rotationDegrees(90.0F));
-		TNTMinecartRenderer.renderTntFlash(Blocks.TNT.getDefaultState(), matrixStackIn, bufferIn, packedLightIn, false);
+		float f = MathHelper.interpolateAngle(partialTicks, entityIn.prevRotationYaw, entityIn.rotationYaw);
+		float f1 = MathHelper.lerp(partialTicks, entityIn.prevRotationPitch, entityIn.rotationPitch);
+		matrixStackIn.translate(0, -0.5, 0);
+		model.setRotationAngles(entityIn, 0, 0, 0, f, f1);
+		IVertexBuilder ivertexbuilder = bufferIn.getBuffer(model.getRenderType(TEXTURE));
+		//From TNT rendering
+		int overlay = entityIn.ticksExisted / 5 % 2 == 0 ? OverlayTexture.getPackedUV(OverlayTexture.getU(1.0F), 10) : OverlayTexture.NO_OVERLAY;
+		model.render(matrixStackIn, ivertexbuilder, packedLightIn, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
 		matrixStackIn.pop();
 		super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
 	}
 
 	@Override
 	public ResourceLocation getEntityTexture(SwampMineEntity entity) {
-		return AtlasTexture.LOCATION_BLOCKS_TEXTURE;
+		return TEXTURE;
 	}
 
 }
