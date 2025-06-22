39451e8bc33e07421aa895a798cb8fc7389e0147
Lykrast <aymericpm@gmail.com>
2025-06-22 17:06:12 +0200

    Added tag to block passage's toll
    Fixes #8

--- src/main/java/lykrast/meetyourfight/item/PassagesToll.java
+++ src/main/java/lykrast/meetyourfight/item/PassagesToll.java
@@ -2,6 +2,7 @@ package lykrast.meetyourfight.item;
 
 import java.util.List;
 
+import lykrast.meetyourfight.MeetYourFight;
 import lykrast.meetyourfight.config.MYFConfigValues;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
@@ -9,6 +10,8 @@ import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -16,8 +19,11 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 
 public class PassagesToll extends Item {
+	public static final TagKey<Block> DENY = BlockTags.create(MeetYourFight.rl("blocks_passages_toll"));
+	
 	public PassagesToll(Properties properties) {
 		super(properties);
 	}
@@ -37,23 +43,26 @@ public class PassagesToll extends Item {
 		//Query the wall to see if there's a hole
 		BlockPos.MutableBlockPos mut = new BlockPos.MutableBlockPos(pos.getX(), pos.getY(), pos.getZ());
 		boolean found = false;
-		for (int i = 0; i < MYFConfigValues.PASSAGES_TOLL_RANGE; i++) {
-			mut.move(dir);
-			//Prevent players getting in the void
-			if (mut.getY() <= world.getMinBuildHeight()) break;
-			//I dunno why blocksmotion is deprecated and I have no sign of what to replace it with
-			if (!world.getBlockState(mut).blocksMotion()) {
-				found = true;
-				//Bring down to player's height as much as possible
-				double targetY = player.getY();
-				while (mut.getY() > targetY && mut.getY() > 1) {
-					mut.move(0, -1, 0);
-					if (world.getBlockState(mut).blocksMotion()) {
-						mut.move(0, 1, 0);
-						break;
+		if (!world.getBlockState(mut).is(DENY)) {
+			for (int i = 0; i < MYFConfigValues.PASSAGES_TOLL_RANGE; i++) {
+				mut.move(dir);
+				//Prevent players getting in the void
+				if (mut.getY() <= world.getMinBuildHeight()) break;
+				if (world.getBlockState(mut).is(DENY)) break;
+				//I dunno why blocksmotion is deprecated and I have no sign of what to replace it with
+				if (!world.getBlockState(mut).blocksMotion()) {
+					found = true;
+					//Bring down to player's height as much as possible
+					double targetY = player.getY();
+					while (mut.getY() > targetY && mut.getY() > 1) {
+						mut.move(0, -1, 0);
+						if (world.getBlockState(mut).blocksMotion()) {
+							mut.move(0, 1, 0);
+							break;
+						}
 					}
+					break;
 				}
-				break;
 			}
 		}
 		
